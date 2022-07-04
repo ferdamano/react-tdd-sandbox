5234973ae8c898d5c7968fbd1a1de1d270d32da3
Ferdinand McDaniel <imcdanielamano@gmail.com>
2022-07-04 09:52:49 +0800

    added fields plust tests suite

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -2,7 +2,17 @@ import { Component } from "react";
 
 class SignUpPage extends Component {
     render(){
-        return <h1 className="hello-world">Sign Up</h1>;
+        return(
+            <div>
+                <h1 className="hello-world">Sign Up</h1>;
+                <label>Username</label>
+                <input placeholder="username" type="text" />
+                <label>E-mail</label>
+                <input placeholder="email" type="text" />
+                <label>Password</label>
+                <input placeholder="password" type="password"/>
+            </div>
+        );
     }
 }
 
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,9 +1,29 @@
 import SignUpPage from "./SignUpPage";
 import { render, screen } from "@testing-library/react"
 
-it("has header", () => {
-    render(<SignUpPage />);
-    const header = screen.queryByRole("heading", { name: "Sign Up"});
-    expect(header).toBeInTheDocument();
-    expect(header).toHaveClass("hello-world");
-})
+describe('Sign Up Page', () => {
+    describe('Layout', () => {
+        it("has header", () => {
+            render(<SignUpPage />);
+            const header = screen.queryByRole("heading", { name: "Sign Up"});
+            expect(header).toBeInTheDocument();
+        })
+        it('has username input', () => {
+            render(<SignUpPage />);
+            const input = screen.getByPlaceholderText("username");
+            expect(input).toBeInTheDocument();
+        });
+        it('has email input', () => {
+            render(<SignUpPage />);
+            const input = screen.getByPlaceholderText("email");
+            expect(input).toBeInTheDocument();
+        });
+        it('has password input', () => {
+            render(<SignUpPage />);
+            const input = screen.getByPlaceholderText("password");
+            expect(input).toBeInTheDocument();
+            expect(input.type).toBe("password");
+        });
+    });
+});
+
